0a9c3c7bc058aecd1b754d1750fdb11aa5978b9d
Dat Truong <ducdat147@gmail.com>
2020-05-27 00:12:53 +0700

    adit some form

--- student/urls.py
+++ student/urls.py
@@ -4,7 +4,7 @@ from .import views
 urlpatterns = [
     path('',views.ListStudent, name='liststudent'),
     #path('<int:pk>/',views.StudentDetailView.as_view(), name='detailstudent'),
-    path('createstudent/',views.CreateStudent,name='create'),
+    # path('<int:pk>/',views.DetailStudent,name='detailstudent'),
     path('upload/',views.upload_file,name='upload'),
     path('base2/',views.test, name='test'),
     # path('class/',views.ListClass,name='listclass'),
@@ -23,7 +23,7 @@ urlpatterns = [
     path('listschedule/',views.ListSchedule,name='listschedule'),
     path('detailschedule/',views.DetailSchedule,name='detailschedule'),
     # Student 
-    path('createstudent/',views.CreateStudent,name='createstudent'),
+    path('create/',views.CreateStudent,name='create'),
     # path('liststudent/',views.ListStudent,name='liststudent'),
     path('<int:pk>/',views.DetailStudent,name='detailstudent'),
     # Teacher 
