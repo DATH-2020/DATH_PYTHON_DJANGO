db43e95ea589d14caa7bc72e2233e42f66f57d44
Dat Truong <ducdat147@gmail.com>
2020-05-30 01:18:21 +0700

    chua post dc

--- home/views.py
+++ home/views.py
@@ -98,19 +98,35 @@ def createStudent(request):
                 fail_silently = False,
             )
             return redirect('liststudent')
-    context = {'form':form, 'gender':gender, 'unit':unit, 'classname':classname}
+    context = { 'gender':gender, 'unit':unit, 'classname':classname}
     return render(request, 'student/createStudent.html', context)
 
 def detailStudent(request,pk):
     student = Student.objects.get(pk=pk)
-    context = {'student':student}
+    classname = Classname.objects.all()
+    unit = Unit.objects.all()
+    form = CreateStudentForm(instance=student)
+    if request.method == 'POST':
+        form = CreateStudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            # send_mail(
+            #     subject = 'Xác nhận đã cập nhật lại thông tin học viên', # title mail
+            #     message = 'Bạn vừa hoàn thành cập nhật thông tin học viên tại HITECH, vui lòng kiểm tra nếu nội dung không chính xác. Xin cảm ơn !', # nội dung mail
+            #     from_email= 'ducdat147@gmail.com', # tài khoản
+            #     auth_password= 'truongducdat123', # mk
+            #     recipient_list = [form.cleaned_data.get('email')],# mail người nhận
+            #     fail_silently = False,
+            # )
+            return redirect('liststudent')
+    context = {'form':form, 'student':student, 'classname': classname, 'unit': unit}
     return render(request,'student\detailStudent.html',context)
 # Teacher 
 # Dat
 @login_required(login_url='login')
 def listTeacher(request):
     listteacher = Teacher.objects.all()
-    context = {'listteacher': listteacher}
+    context = {'listteacher': listteacher, 'classname': classname}
     return render(request, 'teacher/teacher.html', context)
 
 def listStaf(request):
